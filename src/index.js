d1fa1d9d80202f91eb38b44870ebe6b54cff6052
James York <james@grandcircus.co>
2018-02-16 16:15:19 -0500

    formatting

--- src/index.js
+++ src/index.js
@@ -29,10 +29,12 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square 
-      value = { this.state.squares[i] } 
-      onClick = { this.handleClick(i) } 
-    />;
+    return (
+      <Square 
+        value = { this.state.squares[i] } 
+        onClick = { this.handleClick(i) } 
+      />
+    );
   }
 
   render() {
